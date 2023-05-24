c6bcd309eda1de31865de89f1259fe57d946ccf9
bedirhanakinn <bedirhanakinn@gmail.com>
2023-05-24 15:29:01 +0200

    PointSystem

---
+++ Bike Runners True/Assets/PointCollection.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PointCollection : MonoBehaviour
+{
+ 
+private int Coin = 0;
+
+public TextMeshProUGUI coinText;
+
+private void OnTriggerEnter(Collider other)
+{
+    if(other.transform.tag == "Coin")
+    {
+        Coin++;
+        coinText.text = "Coin: " + Coin.ToString();
+        Debug.Log(Coin);
+        Destroy(other.gameObject);
+    }
+}
+
+}
